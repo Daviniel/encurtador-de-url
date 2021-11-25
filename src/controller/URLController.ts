91bd45363e0016aad2031f8ba3f04520e8c3a091
Daviniel <eng.daviniel@gmail.com>
2021-11-25 15:08:27 -0300

    Projeto finalizado

--- src/controller/URLController.ts
+++ src/controller/URLController.ts
@@ -8,17 +8,22 @@ export class URLController {
 
         const { originURL } = req.body
         const url = await URLModel.findOne({ originURL })
+
         if (url) {
             response.json(url)
             return
         }
+
         const hash = shortId.generate()
         const shortURL = `${config.API_URL}/${hash}`
         const newURL = await URLModel.create({ hash, shortURL, originURL })
+
         response.json(newURL)
+
     }
 
     public async redirect(req: Request, response: Response): Promise<void> {
+
         const { hash } = req.params
         const url = await URLModel.findOne({ hash })
 
@@ -28,5 +33,6 @@ export class URLController {
         }
 
         response.status(400).json({error: 'URL not found'})
+        
     }
 }
